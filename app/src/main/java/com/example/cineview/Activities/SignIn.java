7a5c4db43d85ef7866df4fad4851fd08b8232599
dimas <dimas.rasyach@gmail.com>
2025-05-26 18:04:05 +0700

    Detail sign in dan sign up

--- app/src/main/java/com/example/cineview/Activities/SignIn.java
+++ app/src/main/java/com/example/cineview/Activities/SignIn.java
@@ -1,6 +1,12 @@
 package com.example.cineview.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +23,31 @@ public class SignIn extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sig_in);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signin), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+        ImageView togglePasswordVisibility = findViewById(R.id.togglePasswordVisibility);
+
+        togglePasswordVisibility.setOnClickListener(v -> {
+            if (passwordEditText.getTransformationMethod() instanceof PasswordTransformationMethod) {
+                passwordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                togglePasswordVisibility.setImageResource(R.drawable.eyeopen);
+            } else {
+                passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                togglePasswordVisibility.setImageResource(R.drawable.eyeclose);
+            }
+            passwordEditText.setSelection(passwordEditText.length());
+        });
+
+        TextView tvRegister = findViewById(R.id.tvRegister);
+        tvRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(SignIn.this, SignUp.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
         });
     }
 }
--- app/src/main/java/com/example/cineview/Activities/SignUp.java
+++ app/src/main/java/com/example/cineview/Activities/SignUp.java
@@ -1,6 +1,10 @@
 package com.example.cineview.Activities;
 
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +21,44 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signup), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+        ImageView togglePasswordVisibility = findViewById(R.id.togglePasswordVisibility);
+
+        togglePasswordVisibility.setOnClickListener(v -> {
+            if (passwordEditText.getTransformationMethod() instanceof PasswordTransformationMethod) {
+                passwordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                togglePasswordVisibility.setImageResource(R.drawable.eyeopen);
+            } else {
+                passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                togglePasswordVisibility.setImageResource(R.drawable.eyeclose);
+            }
+            passwordEditText.setSelection(passwordEditText.length());
+        });
+
+        EditText confirmPasswordEditText = findViewById(R.id.confirmpasswordEditText);
+        ImageView toggleConfirmPasswordVisibility = findViewById(R.id.toggleConfirmPasswordVisibility);
+
+        toggleConfirmPasswordVisibility.setOnClickListener(v -> {
+            if (confirmPasswordEditText.getTransformationMethod() instanceof PasswordTransformationMethod) {
+                confirmPasswordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                toggleConfirmPasswordVisibility.setImageResource(R.drawable.eyeopen);
+            } else {
+                confirmPasswordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                toggleConfirmPasswordVisibility.setImageResource(R.drawable.eyeclose);
+            }
+            confirmPasswordEditText.setSelection(confirmPasswordEditText.length());
+        });
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
     }
 }
